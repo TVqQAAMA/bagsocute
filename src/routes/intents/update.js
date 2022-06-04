99e8e586133384a1e3dcbbeb5c2b9946260cf183
chinesetingxie <54350929+chinesetingxie@users.noreply.github.com>
2022-06-04 14:58:57 +0800

    .

--- src/routes/intents/update.js
+++ src/routes/intents/update.js
@@ -24,84 +24,91 @@ export async function post({ request }) {
       }
     })
 
-    // update product.json
-    const product = await productReq.json()
-    const productJson = JSON.parse(Buffer.from(product.content, 'base64').toString('ascii'))
-    productJson.qty = parseInt(newQty, 10)
+    if (productReq.ok) {
+      // update product.json
+      const product = await productReq.json()
+      const productJson = JSON.parse(Buffer.from(product.content, 'base64').toString('ascii'))
+      productJson.qty = parseInt(newQty, 10)
 
-    await fetch(productApiUrl, {
-      method: 'PUT',
-      body: JSON.stringify({
-        message: '[skip ci]',
-        commiter: { name: '', email: '' },
-        content: Buffer.from(JSON.stringify(productJson), 'utf-8').toString('base64'),
-        sha: product.sha
-      }),
-      headers: {
-        Authorization: `token ${process.env['GIT']}`
-      }
-    })
-
-    // get existing index.json
-    const indexApiUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/contents/docs/index.json'
-    const indexReq = await fetch(indexApiUrl, {
-      cache: 'no-store',
-      headers: {
-        Authorization: `token ${process.env['GIT']}`
-      }
-    })
-
-    // update index.json
-    const index = await indexReq.json()
-    const indexJson = JSON.parse(Buffer.from(index.content, 'base64').toString('ascii'))
-    indexJson[handle].qty = parseInt(newQty, 10)
-    await fetch(indexApiUrl, {
-      method: 'PUT',
-      body: JSON.stringify({
-        message: '[skip ci]',
-        commiter: { name: '', email: '' },
-        content: Buffer.from(JSON.stringify(indexJson), 'utf-8').toString('base64'),
-        sha: index.sha
-      }),
-      headers: {
-        Authorization: `token ${process.env['GIT']}`
-      }
-    })
-
-    // get existing collection.json(s)
-    const collectionPromises = productJson.tags.map(async (item) => {
-      const collectionUrl = `https://api.github.com/repos/TVqQAAMA/bagsocute/contents/docs/collections/${item}.json`
-      return await fetch(collectionUrl, {
-        cache: 'no-store',
-        headers: {
-          Authorization: `token ${process.env['GIT']}`
-        }
-      })
-    })
-
-    const collectionJson = await Promise.all(collectionPromises)
-
-    // update respective collection.json(s)
-    collectionJson.map(async (o) => {
-      const collectionRes = await o.json()
-      const collection = JSON.parse(Buffer.from(collectionRes.content, 'base64').toString('ascii'))
-      collection.map(async (o) => {
-        if (o.id === productJson.id) {
-          o.qty = parseInt(newQty, 10)
-        }
-      })
-      await fetch(`https://api.github.com/repos/TVqQAAMA/bagsocute/contents/${collectionRes.path}`, {
+      const productUpdate = await fetch(productApiUrl, {
         method: 'PUT',
         body: JSON.stringify({
           message: '[skip ci]',
           commiter: { name: '', email: '' },
-          content: Buffer.from(JSON.stringify(collection), 'utf-8').toString('base64'),
-          sha: collectionRes.sha
+          content: Buffer.from(JSON.stringify(productJson), 'utf-8').toString('base64'),
+          sha: product.sha
         }),
         headers: {
           Authorization: `token ${process.env['GIT']}`
         }
       })
-    })
+
+      if (productUpdate.ok) {
+        // get existing index.json
+        const indexApiUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/contents/docs/index.json'
+        const indexReq = await fetch(indexApiUrl, {
+          cache: 'no-store',
+          headers: {
+            Authorization: `token ${process.env['GIT']}`
+          }
+        })
+
+        if (indexReq.ok) {
+          // update index.json
+          const index = await indexReq.json()
+          const indexJson = JSON.parse(Buffer.from(index.content, 'base64').toString('ascii'))
+          indexJson[handle].qty = parseInt(newQty, 10)
+          const indexUpdate = await fetch(indexApiUrl, {
+            method: 'PUT',
+            body: JSON.stringify({
+              message: '[skip ci]',
+              commiter: { name: '', email: '' },
+              content: Buffer.from(JSON.stringify(indexJson), 'utf-8').toString('base64'),
+              sha: index.sha
+            }),
+            headers: {
+              Authorization: `token ${process.env['GIT']}`
+            }
+          })
+
+          if (indexUpdate.ok) {
+            // get existing collection.json(s)
+            const collectionPromises = productJson.tags.map(async (item) => {
+              const collectionUrl = `https://api.github.com/repos/TVqQAAMA/bagsocute/contents/docs/collections/${item}.json`
+              return await fetch(collectionUrl, {
+                cache: 'no-store',
+                headers: {
+                  Authorization: `token ${process.env['GIT']}`
+                }
+              })
+            })
+
+            const collectionJson = await Promise.all(collectionPromises)
+            // update respective collection.json(s)
+            collectionJson.map(async (o) => {
+              const collectionRes = await o.json()
+              const collection = JSON.parse(Buffer.from(collectionRes.content, 'base64').toString('ascii'))
+              collection.map(async (o) => {
+                if (o.id === productJson.id) {
+                  o.qty = parseInt(newQty, 10)
+                }
+              })
+              await fetch(`https://api.github.com/repos/TVqQAAMA/bagsocute/contents/${collectionRes.path}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                  message: '[skip ci]',
+                  commiter: { name: '', email: '' },
+                  content: Buffer.from(JSON.stringify(collection), 'utf-8').toString('base64'),
+                  sha: collectionRes.sha
+                }),
+                headers: {
+                  Authorization: `token ${process.env['GIT']}`
+                }
+              })
+            })
+          }
+        }
+      }
+    }
   }
 }
