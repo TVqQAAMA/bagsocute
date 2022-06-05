220be8d7b321ec8248f57b93541baf27c0c6a86b
chinesetingxie <54350929+chinesetingxie@users.noreply.github.com>
2022-06-05 10:33:23 +0800

    .

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 async function go(request) {
+  const fetch = request('node-fetch').default
   const v = JSON.parse(request)
   // console.dir(v)
   const newQty = v.data.object.metadata.qty
