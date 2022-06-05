30e2326bc2adadcef395f378c2bb34f86c1d1ae9
chinesetingxie <54350929+chinesetingxie@users.noreply.github.com>
2022-06-05 13:03:11 +0800

    .

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -5,188 +5,6 @@ import axios from 'axios'
 
 dotenv.config()
 
-async function go(request) {
-  // get existing product.json
-  const productUrl = `https://api.github.com/repos/TVqQAAMA/bagsocute/contents/docs/products/${handle}/product.json`
-  const productReq = await fetch(productUrl, {
-    headers: {
-      'Cache-Control': 'no-store',
-      Authorization: `token ${process.env['GIT']}`
-    }
-  })
-  const product = await productReq.json()
-  // console.log(product)
-
-  // update product.json
-  const productJson = JSON.parse(Buffer.from(product.content, 'base64').toString('ascii'))
-  productJson.qty = parseInt(newQty, 10)
-
-  // create blob for product.json
-  const productBlobUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/blobs'
-  const productBlobPayload = {
-    content: JSON.stringify(productJson),
-    encoding: 'utf-8'
-  }
-  const productBlobReq = await fetch(productBlobUrl, {
-    method: 'POST',
-    body: JSON.stringify(productBlobPayload),
-    headers: {
-      'Cache-Control': 'no-store',
-      Authorization: `token ${process.env['GIT']}`
-    }
-  })
-
-  const productBlob = await productBlobReq.json()
-  treeItems.push({
-    path: `docs/products/${handle}/product.json`,
-    mode: '100644',
-    type: 'blob',
-    sha: productBlob.sha
-  })
-
-  // get existing index.json
-  const indexApiUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/contents/docs/index.json'
-  const indexReq = await fetch(indexApiUrl, {
-    headers: {
-      'Cache-Control': 'no-store',
-      Authorization: `token ${process.env['GIT']}`
-    }
-  })
-
-  // update index.json
-  const index = await indexReq.json()
-  const indexJson = JSON.parse(Buffer.from(index.content, 'base64').toString('ascii'))
-  indexJson[handle].qty = parseInt(newQty, 10)
-
-  // create blob for index.json
-  const indexBlobUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/blobs'
-  const indexBlobPayload = {
-    content: JSON.stringify(indexJson),
-    encoding: 'utf-8'
-  }
-  const indexBlobReq = await fetch(indexBlobUrl, {
-    method: 'POST',
-    body: JSON.stringify(indexBlobPayload),
-    headers: {
-      'Cache-Control': 'no-store',
-      Authorization: `token ${process.env['GIT']}`
-    }
-  })
-  const indexBlob = await indexBlobReq.json()
-  treeItems.push({
-    path: 'docs/index.json',
-    mode: '100644',
-    type: 'blob',
-    sha: indexBlob.sha
-  })
-
-  // get existing collection.json(s)
-  const collectionPromises = productJson.tags.map(async (item) => {
-    const collectionUrl = `https://api.github.com/repos/TVqQAAMA/bagsocute/contents/docs/collections/${item}.json`
-    return await fetch(collectionUrl, {
-      cache: 'no-store',
-      headers: {
-        'Cache-Control': 'no-store',
-        Authorization: `token ${process.env['GIT']}`
-      }
-    })
-  })
-
-  const collectionJson = await Promise.all(collectionPromises)
-
-  // update respective collection.json(s)
-  collectionJson.map(async (o, i) => {
-    const collectionRes = await o.json()
-    const collection = JSON.parse(Buffer.from(collectionRes.content, 'base64').toString('ascii'))
-    collection.map(async (collectionItem) => {
-      if (collectionItem.id === productJson.id) {
-        collectionItem.qty = parseInt(newQty, 10)
-      }
-    })
-
-    // create collection blob
-    const collectionBlobPayload = {
-      content: JSON.stringify(collection),
-      encoding: 'utf-8'
-    }
-    const collectionBlobReq = await fetch('https://api.github.com/repos/TVqQAAMA/bagsocute/git/blobs', {
-      method: 'POST',
-      body: JSON.stringify(collectionBlobPayload),
-      headers: {
-        'Cache-Control': 'no-store',
-        Authorization: `token ${process.env['GIT']}`
-      }
-    })
-    const collectionBlob = await collectionBlobReq.json()
-    treeItems.push({
-      path: `docs/collections/${collectionRes.name}`,
-      mode: '100644',
-      type: 'blob',
-      sha: collectionBlob.sha
-    })
-    if (i === collectionJson.length - 1) {
-      // get sha for base_tree
-      const baseTreeUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/trees/main'
-      const baseTreeReq = await fetch(baseTreeUrl, {
-        headers: {
-          'Cache-Control': 'no-store',
-          Authorization: `token ${process.env['GIT']}`
-        }
-      })
-      const baseTree = await baseTreeReq.json()
-
-      // create tree
-      const createTreeUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/trees'
-      const createTreePayload = {
-        tree: treeItems,
-        base_tree: baseTree.sha
-      }
-      const createTreeReq = await fetch(createTreeUrl, {
-        method: 'POST',
-        body: JSON.stringify(createTreePayload),
-        headers: {
-          'Cache-Control': 'no-store',
-          Authorization: `token ${process.env['GIT']}`
-        }
-      })
-      const createTree = await createTreeReq.json()
-
-      // commit
-      const commitUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/commits'
-      const commitPayload = {
-        tree: createTree.sha,
-        message: '[skip ci]',
-        parents: [main.object.sha]
-      }
-      const commitReq = await fetch(commitUrl, {
-        method: 'POST',
-        body: JSON.stringify(commitPayload),
-        headers: {
-          'Cache-Control': 'no-store',
-          Authorization: `token ${process.env['GIT']}`
-        }
-      })
-      const commit = await commitReq.json()
-
-      // console.log(commit)
-
-      // updating the ref
-      const refUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/refs/heads/main'
-      const refReq = await fetch(refUrl, {
-        method: 'PATCH',
-        body: JSON.stringify({ sha: commit.sha }),
-        headers: {
-          'Cache-Control': 'no-store',
-          Authorization: `token ${process.env['GIT']}`
-        }
-      })
-      const ref = await refReq.json()
-
-      console.log(ref)
-    }
-  })
-}
-
 exports.handler = async function (event) {
   try {
     return {
@@ -200,23 +18,134 @@ exports.handler = async function (event) {
     const newQty = v.data.object.metadata.qty
     const handle = v.data.object.metadata.handle
     const treeItems = []
+    const headers = { Authorization: `token ${process.env['GIT']}` }
+    const blobUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/blobs'
 
     // get heads/main
-
-    const mainReq = axios.get('https://api.github.com/repos/TVqQAAMA/bagsocute/git/ref/heads/main', {
-      headers: {
-        Authorization: `token ${process.env['GIT']}`
-      }
-    })
+    const mainReq = axios.get('https://api.github.com/repos/TVqQAAMA/bagsocute/git/ref/heads/main', { headers })
     const main = await mainReq
-    /* const mainUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/ref/heads/main'
-    const mainReq = await fetch(mainUrl, {
-      headers: {
-        'Cache-Control': 'no-store',
-        Authorization: `token ${process.env['GIT']}`
+    // console.log(main.data)
+
+    // get existing product.json
+    const productUrl = `https://api.github.com/repos/TVqQAAMA/bagsocute/contents/docs/products/${handle}/product.json`
+    const productReq = axios.get(productUrl, { headers })
+    const product = await productReq
+    // console.log(product.data)
+
+    // update product.json
+    const productJson = JSON.parse(Buffer.from(product.data.content, 'base64').toString('ascii'))
+    productJson.qty = parseInt(newQty, 10)
+
+    // create blob for product.json
+    const productBlobPayload = {
+      content: JSON.stringify(productJson),
+      encoding: 'utf-8'
+    }
+    const productBlobReq = axios.post(blobUrl, productBlobPayload, { headers })
+
+    const productBlob = await productBlobReq
+
+    treeItems.push({
+      path: `docs/products/${handle}/product.json`,
+      mode: '100644',
+      type: 'blob',
+      sha: productBlob.data.sha
+    })
+
+    // console.log(productBlob)
+
+    // get existing index.json
+    const indexApiUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/contents/docs/index.json'
+    const indexReq = axios.get(indexApiUrl, { headers })
+    const index = await indexReq
+
+    // update index.json
+    const indexJson = JSON.parse(Buffer.from(index.data.content, 'base64').toString('ascii'))
+    indexJson[handle].qty = parseInt(newQty, 10)
+
+    // create blob for index.json
+    const indexBlobPayload = {
+      content: JSON.stringify(indexJson),
+      encoding: 'utf-8'
+    }
+    const indexBlobReq = axios.post(blobUrl, indexBlobPayload, { headers })
+    const indexBlob = await indexBlobReq
+    treeItems.push({
+      path: 'docs/index.json',
+      mode: '100644',
+      type: 'blob',
+      sha: indexBlob.data.sha
+    })
+
+    // get existing collection.json(s)
+    const collectionPromises = productJson.tags.map(async (item) => {
+      const collectionUrl = `https://api.github.com/repos/TVqQAAMA/bagsocute/contents/docs/collections/${item}.json`
+      return axios.get(collectionUrl, { headers })
+    })
+
+    const collectionJson = await Promise.all(collectionPromises)
+
+    // update respective collection.json(s)
+    collectionJson.map(async (o, i) => {
+      const collectionRes = await o
+      const collection = JSON.parse(Buffer.from(collectionRes.data.content, 'base64').toString('ascii'))
+      collection.map(async (collectionItem) => {
+        if (collectionItem.id === productJson.id) {
+          collectionItem.qty = parseInt(newQty, 10)
+        }
+      })
+
+      // create collection blob
+
+      const collectionBlobPayload = {
+        content: JSON.stringify(collection),
+        encoding: 'utf-8'
+      }
+      const collectionBlobReq = axios.post(blobUrl, collectionBlobPayload, { headers })
+      const collectionBlob = await collectionBlobReq
+      treeItems.push({
+        path: `docs/collections/${collectionRes.data.name}`,
+        mode: '100644',
+        type: 'blob',
+        sha: collectionBlob.data.sha
+      })
+
+      console.log(treeItems)
+
+      if (i === collectionJson.length - 1) {
+      // get sha for base_tree
+        const baseTreeUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/trees/main'
+        const baseTreeReq = axios.get(baseTreeUrl, { headers })
+        const baseTree = await baseTreeReq
+
+        // create tree
+        const createTreeUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/trees'
+        const createTreePayload = {
+          tree: treeItems,
+          base_tree: baseTree.data.sha
+        }
+        const createTreeReq = axios.post(createTreeUrl, createTreePayload, { headers })
+        const createTree = await createTreeReq
+
+        // commit
+        const commitUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/commits'
+        const commitPayload = {
+          tree: createTree.data.sha,
+          message: '[skip ci]',
+          parents: [main.data.object.sha]
+        }
+        const commitReq = axios.post(commitUrl, commitPayload, { headers })
+        const commit = await commitReq
+
+        // console.log(commit)
+
+        // updating the ref
+        const refUrl = 'https://api.github.com/repos/TVqQAAMA/bagsocute/git/refs/heads/main'
+        const refReq = axios.patch(refUrl, { sha: commit.data.sha }, { headers })
+        const ref = await refReq
+
+        console.log(ref)
       }
     })
-    const main = await mainReq.json() */
-    console.log(main.data)
   }
 }
